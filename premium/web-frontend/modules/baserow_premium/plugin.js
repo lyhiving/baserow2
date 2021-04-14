01047358b9efda6a1204371b8c1bef0899543ca9
Nigel Gott <nigel@baserow.io>
2021-04-14 11:33:19 +0100

    Include changes hidden by an intellij changelog...

--- premium/web-frontend/modules/baserow_premium/plugin.js
+++ premium/web-frontend/modules/baserow_premium/plugin.js
@@ -1,7 +1,5 @@
 import {PremPlugin} from '@baserow_premium/plugins'
 
 export default ({app}) => {
-    console.log("Hello world from the prem plugin!")
-
     app.$registry.register('plugin', new PremPlugin())
 }
