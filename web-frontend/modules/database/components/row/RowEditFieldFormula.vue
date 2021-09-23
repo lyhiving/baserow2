ba5b87518868b745733d91aa549a0afb4dc418d6
Nigel Gott <nigel@baserow.io>
2021-09-23 15:26:20 +0100

    Add proper formula row edit model and display NaN's nicely in the number row edit field component.

--- web-frontend/modules/database/components/row/RowEditFieldFormula.vue
+++ web-frontend/modules/database/components/row/RowEditFieldFormula.vue
@@ -1,16 +1,32 @@
 <template>
-  <div class="control__elements">
-    <div>
-      {{ value }}
-    </div>
-  </div>
+  <component :is="getComponent(field)" v-bind="$props" read-only></component>
 </template>
 
 <script>
 import rowEditField from '@baserow/modules/database/mixins/rowEditField'
+import RowEditFieldDateReadOnly from '@baserow/modules/database/components/row/RowEditFieldDateReadOnly'
+import RowEditFieldText from '@baserow/modules/database/components/row/RowEditFieldText'
+import RowEditFieldBoolean from '@baserow/modules/database/components/row/RowEditFieldBoolean'
+import RowEditFieldNumber from '@baserow/modules/database/components/row/RowEditFieldNumber'
 
 export default {
   name: 'RowEditFieldFormula',
+  components: {
+    RowEditFieldDateReadOnly,
+    RowEditFieldText,
+    RowEditFieldBoolean,
+    RowEditFieldNumber,
+  },
   mixins: [rowEditField],
+  methods: {
+    getComponent(field) {
+      return {
+        date: RowEditFieldDateReadOnly,
+        text: RowEditFieldText,
+        boolean: RowEditFieldBoolean,
+        number: RowEditFieldNumber,
+      }[field.formula_type]
+    },
+  },
 }
 </script>
