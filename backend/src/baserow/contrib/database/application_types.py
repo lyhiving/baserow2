2f410ed7d9cf5384ffce97ad09c6478ac4cbca11
Nigel Gott <nigel@baserow.io>
2021-11-03 15:47:37 +0000

    Fix import for now

--- backend/src/baserow/contrib/database/application_types.py
+++ backend/src/baserow/contrib/database/application_types.py
@@ -151,10 +151,7 @@ class DatabaseApplicationType(ApplicationType):
             # We don't need to create all the fields individually because the schema
             # editor can handle the creation of the table schema in one go.
             with connection.schema_editor() as schema_editor:
-                model = table["_object"].get_model(
-                    fields=table["_field_objects"],
-                    field_ids=[],
-                )
+                model = table["_object"].get_model()
                 table["_model"] = model
                 schema_editor.create_model(model)
 
